d58696c72115c4169774d46becdc258d5cac0754
Diego Castro <diego.castro@eldorado.org.br>
2015-04-23 14:48:01 -0300

    Added sample code

---
+++ LiveTileUniversalApps/LiveTileUniversalApps/LiveTileUniversalApps.Shared/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace LiveTileUniversalApps
+{    
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CreateLiveTileButton_Click(object sender, RoutedEventArgs e)
+        {
+            // To see all live tiles template, access https://msdn.microsoft.com/en-us/library/windows/apps/xaml/hh761491.aspx           
+
+            var xmlFile = XElement.Load("Tile.xml");
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(xmlFile.ToString());
+
+            TileNotification tileNotification = new TileNotification(xmldoc);
+
+            TileUpdateManager.CreateTileUpdaterForApplication().Clear();            
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+
+            Application.Current.Exit();
+        }
+
+        private void ChangeContentButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(TileContentPage));            
+        }
+
+        private void ChangeTileTemplateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var xmlFile = XElement.Load("NewTile.xml");
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(xmlFile.ToString());
+
+            TileNotification tileNotification = new TileNotification(xmldoc);
+
+            TileUpdateManager.CreateTileUpdaterForApplication().Clear();
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+
+            Application.Current.Exit();
+        }
+    }
+}
---
+++ LiveTileUniversalApps/LiveTileUniversalApps/LiveTileUniversalApps.Shared/TileContentPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace LiveTileUniversalApps
+{    
+    public sealed partial class TileContentPage : Page
+    {
+        public TileContentPage()
+        {
+            this.InitializeComponent();
+        }       
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            var xmlFile = XElement.Load("Tile.xml");
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(xmlFile.ToString());
+
+
+            // Change content of tile
+            foreach (IXmlNode node in xmldoc.GetElementsByTagName("text"))
+            {
+                if (node.ParentNode.Attributes.GetNamedItem("template").InnerText == "TileWide310x150ImageAndText01")
+                {
+                    if (node.Attributes.GetNamedItem("id").InnerText == "1")
+                    {
+                        node.InnerText = this.text1TextBox.Text;
+                    }
+                }
+                else if (node.ParentNode.Attributes.GetNamedItem("template").InnerText == "TileSquare150x150Text02")
+                {
+                    if (node.Attributes.GetNamedItem("id").InnerText == "1")
+                    {
+                        node.InnerText = this.text1TextBox.Text;
+                    }
+                    if (node.Attributes.GetNamedItem("id").InnerText == "2")
+                    {
+                        node.InnerText = this.text2TextBox.Text;
+                    }
+                }
+            }
+
+            TileNotification tileNotification = new TileNotification(xmldoc);
+
+            TileUpdateManager.CreateTileUpdaterForApplication().Clear();
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+
+            Application.Current.Exit();
+        }
+    }
+}
